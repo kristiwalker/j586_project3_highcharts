b1118df08220617682f6a0ed0d4165a12e344744
walkerkd <kristiwalker22@gmail.com>
2015-03-29 12:48:28 -0400

    3rd graph up

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
         var percent = [];
         var Country = [];
         var category = [];
+        var adjustedSpending = [];
 
         
         $(document).ready(function(){ //runs the functions
@@ -142,6 +143,73 @@
                 
         });
         
+                $(document).ready(function(){ //runs the functions
+
+        
+                $.ajax({ //loads in xml file
+                        type: "GET",
+                        url: "spending-per-year/inter-dev-spending-years.xml",
+                        dataType: "xml",
+                        success: parseXML
+                });
+	
+                function parseXML(xml) { 
+                            
+        
+                        $(xml).find('perYear').each(function(){ //starts loop to find all people, etc
+                            //console.log("once for every person");
+                            var $perYear = $(this); 
+                            var year = $perYear.attr("name");
+                            var description = $perYear.find('spending').text();
+                            var imageurl = $perYear.attr('imageurl');
+                            adjustedSpending.push(parseInt($perYear.find('adjustedSpending').text())); //parseInt is a function that says turn this text into an integer. Push adds the data to the back of each one so that order makes sense. Pop goes to the front.
+        
+                        var html = '<dt> <img class="bioImage" alt="" src="' + imageurl + '" /> </dt>';
+                        html += '<dd> <span class="loadingPic" alt="Loading" />';
+                        html += '<p class="name">' + year + '</p>';
+                        html += '<p> ' + description + '</p>' ;
+                        html += '</dd>';
+            
+                        $('dl').append($(html));
+                        
+                    
+                });
+	
+                console.log(adjustedSpending);
+                buildChart(); //finally builds chart -- needs to be inside function but outside loop so it won't try to write the chart 100 times, etc.
+        }
+
+        function buildChart(xml){ //tells how to build chart, but need to add buildChart blah blah in document ready above
+	
+	
+                var chart3 = new Highcharts.Chart({
+                        chart: {
+                            renderTo: 'column2',
+                            type: 'line'
+                        },
+                        title: {
+                            text: 'Foreign Aid Categories'
+                        },
+                        xAxis: {
+                            categories: name
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'Dollars'
+                            }
+                        },
+                        series: [{
+                            name: 'Sector',
+                            data: adjustedSpending
+                        }]
+                });
+	
+	
+
+                }
+                
+                
+        });
         
         
     
