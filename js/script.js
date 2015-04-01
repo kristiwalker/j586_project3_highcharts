eee45b30ce39c6e9be3d110822b7d3e8868e8ec4
walkerkd <kristiwalker22@gmail.com>
2015-04-01 19:05:12 -0400

    Did things

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
         
         var dispursement = [];
-        var percent = [];
-        var Country = [];
+        var country = [];
         var category = [];
         var adjustedSpending = [];
+        var place =[];
+        var year =[];
+        var time =[];
 
         
         $(document).ready(function(){ //runs the functions
@@ -22,15 +24,15 @@
                         $(xml).find('foraid').each(function(){ //starts loop to find all people, etc
                             //console.log("once for every person");
                             var $foraid = $(this); 
-                            var country = $foraid.attr("name");
+                            var place = $foraid.attr("name");
                             var description = $foraid.find('country').text();
                             var imageurl = $foraid.attr('imageurl');
                             dispursement.push(parseInt($foraid.find('dispursement').text())); //parseInt is a function that says turn this text into an integer. Push adds the data to the back of each one so that order makes sense. Pop goes to the front.
-                        
+                            country.push($foraid.find('country').text()); //parseInt is a function that says turn this text into an integer. Push adds the data to the back of each one so that order makes sense. Pop goes to the front.
         
                         var html = '<dt> <img class="bioImage" alt="" src="' + imageurl + '" /> </dt>';
                         html += '<dd> <span class="loadingPic" alt="Loading" />';
-                        html += '<p class="name">' + country + '</p>';
+                        html += '<p class="name">' + place + '</p>';
                         html += '<p> ' + description + '</p>' ;
                         html += '</dd>';
             
@@ -45,8 +47,10 @@
 
         function buildChart(xml){ //tells how to build chart, but need to add buildChart blah blah in document ready above
 	
+        console.log(country);
 	
                 var chart1 = new Highcharts.Chart({
+                        colors: ['#3e7fa3'],
                         chart: {
                             renderTo: 'column1',
                             type: 'column'
@@ -55,7 +59,7 @@
                             text: 'Top 10 countries that recieve most aid from U.S.'
                         },
                         xAxis: {
-                            categories: name
+                            categories: country
                         },
                         yAxis: {
                             title: {
@@ -93,8 +97,6 @@
                             var categories = $sector.attr("name");
                             var description = $sector.find('category').text();
                             var imageurl = $sector.attr('imageurl');
-                            percent.push(parseInt($sector.find('percent').text())); //parseInt is a function that says turn this text into an integer. Push adds the data to the back of each one so that order makes sense. Pop goes to the front.
-                            category.push(parseInt($sector.find('category').text())); //parseInt is a function that says turn this text into an integer. Push adds the data to the back of each one so that order makes sense. Pop goes to the front.
         
                         var html = '<dt> <img class="bioImage" alt="" src="' + imageurl + '" /> </dt>';
                         html += '<dd> <span class="loadingPic" alt="Loading" />';
@@ -107,14 +109,14 @@
                     
                 });
 	
-                console.log(percent);
                 buildChart(); //finally builds chart -- needs to be inside function but outside loop so it won't try to write the chart 100 times, etc.
         }
 
         function buildChart(xml){ //tells how to build chart, but need to add buildChart blah blah in document ready above
 	
-	
+                console.log(category);
                 var chart2 = new Highcharts.Chart({
+                        colors: ['#3e7fa3'],
                         chart: {
                             renderTo: 'pie1',
                             type: 'pie'
@@ -122,17 +124,32 @@
                         title: {
                             text: 'Foreign Aid Categories'
                         },
-                        xAxis: {
-                            categories: name
-                        },
-                        yAxis: {
-                            title: {
-                                text: 'Dollars'
+                        
+                        plotOptions: {
+                            pie: {
+                                allowPointSelect: true,
+                                cursor: 'pointer',
+                                dataLabels: {
+                                    enabled: true
+                                },
+                                showInLegend: false
                             }
-                        },
+                        },             
+                        
+                            
                         series: [{
                             name: 'Sector',
-                            data: percent
+                            data: [
+                            ['Democracy and Governance',828915298],
+                            ['Economic Development',2153869585],
+                            ['Education and Social Services',893254312],
+                            ['Environment',211275166],
+                            ['Health',2605956145],
+                            ['Humanitarian Assistance',533328557],
+                            ['Peace and Security',308706778],
+                            ['Program Management',334241629],
+
+                            ]
                         }]
                 });
 	
@@ -159,14 +176,15 @@
                         $(xml).find('perYear').each(function(){ //starts loop to find all people, etc
                             //console.log("once for every person");
                             var $perYear = $(this); 
-                            var year = $perYear.attr("name");
+                            var time = $perYear.attr("name");
                             var description = $perYear.find('spending').text();
                             var imageurl = $perYear.attr('imageurl');
                             adjustedSpending.push(parseInt($perYear.find('adjustedSpending').text())); //parseInt is a function that says turn this text into an integer. Push adds the data to the back of each one so that order makes sense. Pop goes to the front.
-        
+                            year.push($perYear.find('year').text()); //parseInt is a function that says turn this text into an integer. Push adds the data to the back of each one so that order makes sense. Pop goes to the front.
+
                         var html = '<dt> <img class="bioImage" alt="" src="' + imageurl + '" /> </dt>';
                         html += '<dd> <span class="loadingPic" alt="Loading" />';
-                        html += '<p class="name">' + year + '</p>';
+                        html += '<p class="name">' + time + '</p>';
                         html += '<p> ' + description + '</p>' ;
                         html += '</dd>';
             
@@ -183,6 +201,7 @@
 	
 	
                 var chart3 = new Highcharts.Chart({
+                        colors: ['#3e7fa3'],
                         chart: {
                             renderTo: 'column2',
                             type: 'line'
@@ -191,7 +210,7 @@
                             text: 'Foreign Aid Categories'
                         },
                         xAxis: {
-                            categories: name
+                            categories: year
                         },
                         yAxis: {
                             title: {
@@ -201,7 +220,7 @@
                         series: [{
                             name: 'Sector',
                             data: adjustedSpending
-                        }]
+                        }],
                 });
 	
 	
@@ -212,5 +231,9 @@
         });
         
         
-    
-    
+        $(document).ready(function() {
+            $('#example').dataTable( {
+                "ajax": 'dataTable.json'
+            } );
+        } );
+            
